d1b27598f5b9fc28fbc8772cc83b5f72707717bd
Mada Aryakusumah <lokermada@gmail.com>
2018-04-17 14:34:15 +0700

    fix javascript

--- app/models/property_invesment_form.rb
+++ app/models/property_invesment_form.rb
@@ -2,18 +2,18 @@ class PropertyInvesmentForm < ActiveRecord::Base
 	validates :property_value, :loan_amount, :interest_rate, :capital_growth, :taxable_income, :proposed_rent, presence: true
 
 	def property_value=(value)
-	   self[:property_value] =  value.to_s.gsub(/\./mi, '')
+	   self[:property_value] =  value.to_s.gsub(/\,/mi, '')
 	end
 
 	def loan_amount=(value)
-	   self[:loan_amount] =  value.to_s.gsub(/\./mi, '')
+	   self[:loan_amount] =  value.to_s.gsub(/\,/mi, '')
 	end
 
 	def taxable_income=(value)
-	   self[:taxable_income] =  value.to_s.gsub(/\./mi, '')
+	   self[:taxable_income] =  value.to_s.gsub(/\,/mi, '')
 	end
 
 	def proposed_rent=(value)
-	   self[:proposed_rent] =  value.to_s.gsub(/\./mi, '')
+	   self[:proposed_rent] =  value.to_s.gsub(/\,/mi, '')
 	end
 end
